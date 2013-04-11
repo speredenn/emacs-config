85bd1c8fafae9fc59a30d63fc069a71dd8cac768
Jean-Baptiste Carré (speredenn) <jbc@0x1d.org>
2013-04-11 02:03:32 +0200

    Added wanderlust + dependencies.

--- el-get/.status.el
+++ el-get/.status.el
@@ -1,57 +1,142 @@
-((auctex status "installed" recipe
-	 (:name auctex :website "http://www.gnu.org/software/auctex/" :description "AUCTeX is an extensible package for writing and formatting TeX files in GNU Emacs and XEmacs. It supports many different TeX macro packages, including AMS-TeX, LaTeX, Texinfo, ConTeXt, and docTeX (dtx files)." :type cvs :module "auctex" :url ":pserver:anonymous@cvs.sv.gnu.org:/sources/auctex" :build
-		`(("./autogen.sh")
-		  ("./configure" "--without-texmf-dir" "--with-lispdir=`pwd`" ,(concat "--with-emacs=" el-get-emacs))
-		  "make")
-		:load-path
-		("." "preview")
-		:load
-		("tex-site.el" "preview/preview-latex.el")
-		:info "doc"))
+((apel status "installed" recipe
+       (:name apel :website "http://www.kanji.zinbun.kyoto-u.ac.jp/~tomo/elisp/APEL/" :description "APEL (A Portable Emacs Library) is a library to support to write portable Emacs Lisp programs." :type github :pkgname "wanderlust/apel" :build
+              (mapcar
+               (lambda
+                 (target)
+                 (list el-get-emacs
+                       (split-string "-batch -q -no-site-file -l APEL-MK -f")
+                       target "prefix" "site-lisp" "site-lisp"))
+               '("compile-apel" "install-apel"))
+              :load-path
+              ("site-lisp/apel" "site-lisp/emu")))
+ (auctex status "installed" recipe
+         (:name auctex :website "http://www.gnu.org/software/auctex/" :description "AUCTeX is an extensible package for writing and formatting TeX files in GNU Emacs and XEmacs. It supports many different TeX macro packages, including AMS-TeX, LaTeX, Texinfo, ConTeXt, and docTeX (dtx files)." :type cvs :module "auctex" :url ":pserver:anonymous@cvs.sv.gnu.org:/sources/auctex" :build
+                `(("./autogen.sh")
+                  ("./configure" "--without-texmf-dir" "--with-lispdir=`pwd`" ,(concat "--with-emacs=" el-get-emacs))
+                  "make")
+                :load-path
+                ("." "preview")
+                :load
+                ("tex-site.el" "preview/preview-latex.el")
+                :info "doc"))
  (el-get status "installed" recipe
-	 (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "4.stable" :pkgname "dimitri/el-get" :info "." :load "el-get.el"))
+         (:name el-get :website "https://github.com/dimitri/el-get#readme" :description "Manage the external elisp bits and pieces you depend upon." :type github :branch "4.stable" :pkgname "dimitri/el-get" :info "." :load "el-get.el"))
+ (fixme-mode status "installed" recipe
+             (:name fixme-mode :auto-generated t :type emacswiki :description "Makes FIXME, TODO, etc. appear in big, angry letters" :website "https://raw.github.com/emacsmirror/emacswiki.org/master/fixme-mode.el"))
+ (flim status "installed" recipe
+       (:name flim :description "A library to provide basic features about message representation or encoding" :depends apel :type github :branch "flim-1_14-wl" :pkgname "wanderlust/flim" :build
+              (mapcar
+               (lambda
+                 (target)
+                 (list el-get-emacs
+                       (mapcar
+                        (lambda
+                          (pkg)
+                          (mapcar
+                           (lambda
+                             (d)
+                             `("-L" ,d))
+                           (el-get-load-path pkg)))
+                        '("apel"))
+                       (split-string "-batch -q -no-site-file -l FLIM-MK -f")
+                       target "prefix" "site-lisp" "site-lisp"))
+               '("compile-flim" "install-flim"))
+              :load-path
+              ("site-lisp/flim")))
  (gh status "installed" recipe
      (:name gh :type github :pkgname "sigma/gh.el" :depends
-	    (pcache logito)
-	    :description "Github API client libraries" :website "http://github.com/sigma/gh.el"))
+            (pcache logito)
+            :description "Github API client libraries" :website "http://github.com/sigma/gh.el"))
  (gist status "installed" recipe
        (:name gist :type github :pkgname "defunkt/gist.el" :depends
-	      (gh tabulated-list)
-	      :description "Emacs integration for gist.github.com" :website "http://github.com/defunkt/gist.el"))
+              (gh tabulated-list)
+              :description "Emacs integration for gist.github.com" :website "http://github.com/defunkt/gist.el"))
  (logito status "installed" recipe
-	 (:name logito :type github :pkgname "sigma/logito" :description "logging library for Emacs" :website "http://github.com/sigma/logito"))
+         (:name logito :type github :pkgname "sigma/logito" :description "logging library for Emacs" :website "http://github.com/sigma/logito"))
  (markdown-mode status "installed" recipe
-		(:name markdown-mode :description "Major mode to edit Markdown files in Emacs" :type git :url "git://jblevins.org/git/markdown-mode.git" :before
-		       (add-to-list 'auto-mode-alist
-				    '("\\.\\(md\\|mdown\\|markdown\\)\\'" . markdown-mode))))
+                (:name markdown-mode :description "Major mode to edit Markdown files in Emacs" :type git :url "git://jblevins.org/git/markdown-mode.git" :before
+                       (add-to-list 'auto-mode-alist
+                                    '("\\.\\(md\\|mdown\\|markdown\\)\\'" . markdown-mode))))
  (mu4e status "installed" recipe
        (:name mu4e :website "http://www.djcbsoftware.nl/code/mu/mu4e.html" :description "An emacs-based e-mail client which uses mu (http://www.djcbsoftware.nl/code/mu/) as its back-end: mu4e." :type github :pkgname "djcb/mu" :post-init
-	      (setq mu4e-mu-binary
-		    (expand-file-name "mu"
-				      (expand-file-name "mu"
-							(el-get-package-directory 'mu4e))))
-	      :build
-	      `(("autoreconf -i")
-		("./configure")
-		("make"))
-	      :load-path "mu4e"))
+              (setq mu4e-mu-binary
+                    (expand-file-name "mu"
+                                      (expand-file-name "mu"
+                                                        (el-get-package-directory 'mu4e))))
+              :build
+              `(("autoreconf -i")
+                ("./configure")
+                ("make"))
+              :load-path "mu4e"))
  (pcache status "installed" recipe
-	 (:name pcache :type github :pkgname "sigma/pcache" :description "persistent caching for Emacs" :website "http://github.com/sigma/pcache"))
+         (:name pcache :type github :pkgname "sigma/pcache" :description "persistent caching for Emacs" :website "http://github.com/sigma/pcache"))
+ (semi status "installed" recipe
+       (:name semi :description "SEMI is a library to provide MIME feature for GNU Emacs." :depends flim :type github :branch "semi-1_14-wl" :pkgname "wanderlust/semi" :build
+              (mapcar
+               (lambda
+                 (target)
+                 (list el-get-emacs
+                       (mapcar
+                        (lambda
+                          (pkg)
+                          (mapcar
+                           (lambda
+                             (d)
+                             `("-L" ,d))
+                           (el-get-load-path pkg)))
+                        '("apel" "flim"))
+                       (split-string "-batch -q -no-site-file -l SEMI-MK -f")
+                       target "prefix" "site-lisp" "site-lisp"))
+               '("compile-semi" "install-semi"))
+              :load-path
+              ("site-lisp/semi/")))
  (smex status "installed" recipe
        (:name smex :description "M-x interface with Ido-style fuzzy matching." :type github :pkgname "nonsequitur/smex" :features smex :post-init
-	      (smex-initialize)))
+              (smex-initialize)))
  (tabulated-list status "installed" recipe
-		 (:name tabulated-list :type github :pkgname "sigma/tabulated-list.el" :description "generic major mode for tabulated lists." :website "http://github.com/sigma/tabulated-list.el"))
+                 (:name tabulated-list :type github :pkgname "sigma/tabulated-list.el" :description "generic major mode for tabulated lists." :website "http://github.com/sigma/tabulated-list.el"))
  (tuareg-mode status "installed" recipe
-	      (:name tuareg-mode :type svn :url "svn://svn.forge.ocamlcore.org/svn/tuareg/trunk" :description "A  GOOD Emacs mode to edit Objective Caml code." :load-path
-		     (".")
-		     :prepare
-		     (progn
-		       (autoload 'tuareg-mode "tuareg" "Major mode for editing Caml code" t)
-		       (autoload 'camldebug "camldebug" "Run the Caml debugger" t)
-		       (dolist
-			   (ext
-			    '(".cmo" ".cmx" ".cma" ".cmxa" ".cmi"))
-			 (add-to-list 'completion-ignored-extensions ext))
-		       (add-to-list 'auto-mode-alist
-				    '("\\.ml[iylp]?" . tuareg-mode))))))
+              (:name tuareg-mode :type svn :url "svn://svn.forge.ocamlcore.org/svn/tuareg/trunk" :description "A  GOOD Emacs mode to edit Objective Caml code." :load-path
+                     (".")
+                     :prepare
+                     (progn
+                       (autoload 'tuareg-mode "tuareg" "Major mode for editing Caml code" t)
+                       (autoload 'camldebug "camldebug" "Run the Caml debugger" t)
+                       (dolist
+                           (ext
+                            '(".cmo" ".cmx" ".cma" ".cmxa" ".cmi"))
+                         (add-to-list 'completion-ignored-extensions ext))
+                       (add-to-list 'auto-mode-alist
+                                    '("\\.ml[iylp]?" . tuareg-mode)))))
+ (wanderlust status "installed" recipe
+             (:name wanderlust :description "Wanderlust bootstrap." :depends semi :type github :pkgname "wanderlust/wanderlust" :build
+                    (mapcar
+                     (lambda
+                       (target-and-dirs)
+                       (list el-get-emacs
+                             (mapcar
+                              (lambda
+                                (pkg)
+                                (mapcar
+                                 (lambda
+                                   (d)
+                                   `("-L" ,d))
+                                 (el-get-load-path pkg)))
+                              (append
+                               '("apel" "flim" "semi")
+                               (when
+                                   (el-get-package-exists-p "bbdb")
+                                 (list "bbdb"))))
+                             "--eval"
+                             (el-get-print-to-string
+                              '(progn
+                                 (setq wl-install-utils t)
+                                 (setq wl-info-lang "en")
+                                 (setq wl-news-lang "en")))
+                             (split-string "-batch -q -no-site-file -l WL-MK -f")
+                             target-and-dirs))
+                     '(("wl-texinfo-format" "doc")
+                       ("compile-wl-package" "site-lisp" "icons")
+                       ("install-wl-package" "site-lisp" "icons")))
+                    :info "doc/wl.info" :load-path
+                    ("site-lisp/wl" "utils"))))
